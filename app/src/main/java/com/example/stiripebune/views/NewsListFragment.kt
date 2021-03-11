137d2f2196573180e67f80e1090e7a9ba842866e
hempsader <ionut.grigore@hotmail.com>
2021-03-11 14:18:07 +0200

    Initial commit

---
+++ app/src/main/java/com/example/stiripebune/views/NewsListFragment.kt
@@ -0,0 +1,8 @@
+package com.example.stiripebune.views
+
+import androidx.fragment.app.Fragment
+import com.example.stiripebune.R
+
+class NewsListFragment: Fragment(R.layout.news_recycler_fragment) {
+
+}
